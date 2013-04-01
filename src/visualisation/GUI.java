875b9dabcb478de39f0287eb10ff6574dc4549bc
Stefan Pante <stefanpante@outlook.com>
2013-04-01 23:31:32 +0200

    FIxed search results

--- src/visualisation/GUI.java
+++ src/visualisation/GUI.java
@@ -26,6 +26,7 @@ import controlP5.Slider;
 import controlP5.Textfield;
 
 import processing.core.*;
+import tutorial.Tutorial;
 import visualisation.graph.CircularLayout;
 import visualisation.graph.Connection;
 import visualisation.graph.Graph;
@@ -64,6 +65,7 @@ public class GUI extends PApplet{
 	 */
 	public Transform transform;
 
+
 	/**
 	 * Setup sets up the program for running.
 	 * 	-inits GUI
@@ -100,6 +102,8 @@ public class GUI extends PApplet{
 
 	}
 
+
+
 	SearchResultMenu menu;
 
 	/**
@@ -108,7 +112,7 @@ public class GUI extends PApplet{
 	private void initGraph(){
 		// Gives the parentNode its default appearance
 		initParentNode();
-		
+
 		// Sets the default graphLayout for the graph
 		graph.setGraphLayout(graphLayout);
 		//graph.setGraphLayout(new CircularLayout(graph));
@@ -139,16 +143,21 @@ public class GUI extends PApplet{
 		System.out.println(id);
 		if(id == -1){
 			inputField.setLabel("Search");
-			
+
 		}
 		else{
 			try {
 				System.out.println("Build graph");
 				System.out.println(id);
-				this.graph = GraphFactory.getInstance().fromDatabaseID(id, 1, this);
-				this.graph.setGraphLayout(new RegularForceBasedLayout(graph));
-				System.out.println(graph.getNodes().size());
-				System.out.println("ParentNode not null");
+				Graph newGraph = GraphFactory.getInstance().fromDatabaseID(id, 1, this);
+				if(newGraph.getNodes().size() > 0){
+					graph = newGraph;
+					this.graph.setGraphLayout(new RegularForceBasedLayout(graph));
+				}
+				else{
+					warning = "The Paper does not have enough citations in the database to display";
+					currentFrameWarning = 0;
+				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -158,164 +167,184 @@ public class GUI extends PApplet{
 	}
 
 
-public void mouseReleased(){
-	locked = false;
-}
-
-public void mouseDragged(){
-	if(locked){
-		transform.translationX = (mouseX - xOffset);
-		transform.translationY = (mouseY - yOffset);
-	}
-}
-
-private boolean locked = false;
-private float xOffset;
-private float yOffset;
-
-/**
- * Initializes the parent node of the graph.
- */
-private void initParentNode(){
-
-	// Sets the diameter of the parent node to 50 pixels, replace with static method?
-	//graph.getParentNode().setDiameter(50);
-
-	// Sets the color of the parent node to red.
-	graph.getParentNode().setColor(color(255,0,0));
-}
-
-
-
-/**
- * Setups the gui for this application
- */
-private void setupGUI(){
-
-
-	inputController = new ControlP5(this);
-	int width = displayWidth/2;
-	inputField = inputController.addTextfield("search",  displayWidth/2 - width/2, 15, width, 30 );
-	CColor color = new CColor(color(0, 146, 211), color(255),color(0, 60, 255),color(0, 146, 211),color(0, 146, 211) );
-
-	inputField.setColor(color);
-	inputField.registerTooltip("Type here to search for a paper");
-
-	Bang pause = inputController.addBang("pause");
-
-
-	pause.setColor(color);
-	pause.registerTooltip("With this button you can pause the animation of the graph");
-	pause.setHeight(30);
-	pause.setWidth(30);
-	pause.setPosition(displayWidth/2 - displayWidth/4 - 40, 15);
-
-
-	CColor col = new CColor(color(0, 146, 211), color(0, 60, 255),color(0, 146, 211),color(0, 146, 211),color(0, 146, 211) );
-	slider = inputController.addSlider("zoom");
-	slider.setHeight(displayHeight/2);
-	slider.setWidth(10);
-	slider.setPosition(displayWidth -50, displayHeight/2 - displayHeight/4 );
-	slider.setColor(col);
-	slider.setValue(100f);
-	slider.setRange(5f, 200f);
-	slider.registerTooltip("Use this slider to zoom in or out");
-	slider.getValueLabel().setColor(this.color(255));
-
-}
-Textfield inputField;
-Slider slider;
-float zoom = 100;
-boolean menuEnabled = false;
-SearchResultMenu menu2;
-public void search(String search) throws ParseException, IOException, SQLException{
-	SearchResult[] results = IndexSearcher.generalSearch(search, 10);
-	if(results.length > 0){
-		menuEnabled = true;
-		System.out.println(results.length);
-		menu2 = new SearchResultMenu(results, this);
-		menu2.setStartPositionY(44);
+	public void mouseReleased(){
+		locked = false;
 	}
 
-}
-/**
- * The draw method. is called 25 times/second
- */
-public void draw(){
+	public void mouseDragged(){
+		if(locked){
+			transform.translationX = (mouseX - xOffset);
+			transform.translationY = (mouseY - yOffset);
+		}
+	}
 
-	background(color(255));
-	fill(color(128,0,0),75);
-	transform.scale = zoom/100f;
-	this.ellipse(displayWidth/2 + transform.translationX, displayHeight/2 +transform.translationY,50, 50 );
-	noStroke();
-	if(!fixed){
-		graph.draw();
-		if(!pause){
-			if(graph != null)
-				graph.layout();
+	private boolean locked = false;
+	private float xOffset;
+	private float yOffset;
+
+	/**
+	 * Initializes the parent node of the graph.
+	 */
+	private void initParentNode(){
+
+		// Sets the diameter of the parent node to 50 pixels, replace with static method?
+		//graph.getParentNode().setDiameter(50);
+
+		// Sets the color of the parent node to red.
+		graph.getParentNode().setColor(color(255,0,0));
+	}
+
+
+
+	private boolean guiIsSet = false;
+	/**
+	 * Setups the gui for this application
+	 */
+	private void setupGUI(){
+
+
+		inputController = new ControlP5(this);
+		int width = displayWidth/2;
+		inputField = inputController.addTextfield("search",  displayWidth/2 - width/2, 15, width, 30 );
+		CColor color = new CColor(color(0, 146, 211), color(255),color(0, 60, 255),color(0, 146, 211),color(0, 146, 211) );
+
+		inputField.setColor(color);
+		inputField.registerTooltip("Type here to search for a paper");
+
+		Bang pause = inputController.addBang("pause");
+
+
+		pause.setColor(color);
+		pause.registerTooltip("With this button you can pause the animation of the graph");
+		pause.setHeight(30);
+		pause.setWidth(30);
+		pause.setPosition(displayWidth/2 - displayWidth/4 - 40, 15);
+
+
+		CColor col = new CColor(color(0, 146, 211), color(0, 60, 255),color(0, 146, 211),color(0, 146, 211),color(0, 146, 211) );
+		slider = inputController.addSlider("zoom");
+		slider.setHeight(displayHeight/2);
+		slider.setWidth(10);
+		slider.setPosition(displayWidth -50, displayHeight/2 - displayHeight/4 );
+		slider.setColor(col);
+		slider.setValue(100f);
+		slider.setRange(5f, 200f);
+		slider.registerTooltip("Use this slider to zoom in or out");
+		slider.getValueLabel().setColor(this.color(255));
+		guiIsSet = true;
+
+	}
+	Textfield inputField;
+	Slider slider;
+	float zoom = 100;
+	boolean menuEnabled = false;
+	SearchResultMenu menu2;
+	public void search(String search) throws ParseException, IOException, SQLException{
+		SearchResult[] results = IndexSearcher.generalSearch(search, 10);
+		if(results.length > 0){
+			menuEnabled = true;
+			System.out.println(results.length);
+			menu2 = new SearchResultMenu(results, this);
+			menu2.setStartPositionY(44);
 		}
 
-		updateStatusMessage();
-		if(menuEnabled){
-			menu2.draw();
-			inputField.setLabel("");
-			menu2.hit(mouseX, mouseY);
+	}
+
+
+	/**
+	 * The draw method. is called 25 times/second
+	 */
+	public void draw(){
+		background(color(255));
+		fill(color(128,0,0),75);
+		transform.scale = zoom/100f;
+		this.ellipse(displayWidth/2 + transform.translationX, displayHeight/2 +transform.translationY,50, 50 );
+		noStroke();
+		if(!fixed){
+			graph.draw();
+			if(!pause){
+				if(graph != null)
+					graph.layout();
+			}
+
+			updateStatusMessage();
+			if(menuEnabled){
+				menu2.draw();
+				inputField.setLabel("");
+				menu2.hit(mouseX, mouseY);
+			}
+			else{
+				if(graph != null)
+					graph.hit(mouseX,mouseY);
+			}
 		}
+		
+		displayWarning();
+	}
+
+
+
+	/**
+	 * Updates the status message of this application
+	 */
+	private void updateStatusMessage(){
+		// The basic string
+		String paused = "Program status: ";
+		if(pause){
+			paused += "Paused";
+		}else{
+			paused += "Running";
+		}
+
+		textSize(15);
+		fill(color(0, 146, 211));
+		textAlign(this.LEFT);
+		//textFont(font);
+		text(paused, displayWidth / 2 + displayWidth / 4 + 10, 35);
+	}
+
+	public void pause(){
+		if(pause){
+			pause = false;
+		}
+
 		else{
-			if(graph != null)
-				graph.hit(mouseX,mouseY);
+			pause = true;
 		}
 	}
 
-
-
-}
-
-/**
- * Updates the status message of this application
- */
-private void updateStatusMessage(){
-	// The basic string
-	String paused = "Program status: ";
-	if(pause){
-		paused += "Paused";
-	}else{
-		paused += "Running";
+	boolean fixed = false;
+	public void setFixed(boolean fixed){
+		this.fixed = fixed;
 	}
 
-	textSize(15);
-	fill(color(0, 146, 211));
-	textAlign(this.LEFT);
-	//textFont(font);
-	text(paused, displayWidth / 2 + displayWidth / 4 + 10, 35);
-}
-
-public void pause(){
-	if(pause){
-		pause = false;
+	public boolean getFixed(){
+		return fixed;
 	}
 
-	else{
-		pause = true;
+
+	public Transform getTransform(){
+		return transform;
 	}
-}
 
-boolean fixed = false;
-public void setFixed(boolean fixed){
-	this.fixed = fixed;
-}
-
-public boolean getFixed(){
-	return fixed;
-}
-
-
-public Transform getTransform(){
-	return transform;
-}
-
-public boolean sketchFullScreen(){
-	return true;
-}
+	public boolean sketchFullScreen(){
+		return true;
+	}
+	private String warning;
+	private int currentFrameWarning = 180;
+	private int endFrameWarning = 180;
+	public void displayWarning(){
+		if(currentFrameWarning < endFrameWarning){
+			fill(color(255));
+			stroke(0, 50);
+			rect( displayWidth/2 - 150, displayHeight/2 - 40,300,80);
+			textSize(15);
+			fill(color(0, 60, 255));
+			noStroke();
+			textAlign(CENTER, CENTER);
+			text(warning, displayWidth/2 - 150, displayHeight/2 - 40, 300, 80);
+			currentFrameWarning++;
+		}
+	}
 
 }
